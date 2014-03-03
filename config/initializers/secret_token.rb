0326e35c3a9ad0e0f993981d72981da846a2e0f6
Boris Paillard <boris.paillard@gmail.com>
2014-03-03 13:51:24 +0100

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookConnect::Application.config.secret_key_base = '6e75211bd90841d4748f9dc69aea11aa960d77cc778de936e76ab2f6b7e5fe8ae7c565e91d4f0dd16d3fe2a67e04e5c15235ec97e20733eac34635a7b7b85f77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FacebookConnect::Application.config.session_store :cookie_store, key: '_facebook-connect_session'
